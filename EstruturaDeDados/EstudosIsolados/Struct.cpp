76046e1686f88b464ff5ee5d0811cbafd0d0df83
Anderson Conceição <andersonconceiicao@gmail.com>
2020-10-03 22:51:00 -0300

    inserindo estudo mais aprofundado de ponteiros com structs

---
+++ EstruturaDeDados/EstudosIsolados/Struct.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+// STRUCT USANDO PONTEIRO Usando ponteiro 
+struct moto
+{
+	string cor;
+	string marca;
+	float valor;
+};
+
+int main(int argc, char** argv)
+{
+	moto m;
+	moto *p;
+	
+	m.cor = "branca";
+	m.marca = "fazer";
+	m.valor = 13000.00;
+	
+	p = &m;
+	
+	cout << "Cor "<< p->cor << " Marca: " << p->marca<<" Valor: " << p->valor << " feito com ponteiro"<<endl;
+	
+	return 0;
+}
+
+*/
+
+/*
+
+	// Usando Struct de forma simples. 
+	
+	* Não esqueça de declarar os namespace, para poder usar os tipos de dados corretos.
+struct nome
+{
+	string name;
+	string lastName;	
+};
+
+int main(int argc, char** argv)
+{
+	nome n;
+	
+	n.name = "Anderson";
+	n.lastName = "Conceicao";
+	
+	cout << "Meu nome er " << n.name << " " << n.lastName << endl;
+	
+	return 0;
+}
+*/
