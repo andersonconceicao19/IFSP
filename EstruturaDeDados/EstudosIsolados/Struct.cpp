137c2bc9ed8784d5bcff434327539caa03ecf170
Anderson Conceição <andersonconceiicao@gmail.com>
2020-10-04 00:39:58 -0300

    add algumas duvidas a mais que eu tinha

--- EstruturaDeDados/EstudosIsolados/Struct.cpp
+++ EstruturaDeDados/EstudosIsolados/Struct.cpp
@@ -2,6 +2,61 @@
 
 using namespace std;
 
+
+struct data
+{
+	int dia;
+	int mes;
+	int ano;
+};
+
+int main(int argc, char** argv)
+{
+
+
+	
+	return 0;
+}
+
+/*
+	-- passando valores para a função que deseja colocar na struct
+struct data
+{
+	int dia;
+	int mes;
+	int ano;
+};
+
+void diaDeHoje(data d);
+void init(data *d);
+void criarData(data *d, int dia, int m, int a);
+
+int main(int argc, char** argv)
+{
+	data data;
+	
+	//init(&data);	
+	criarData(&data, 25, 05, 1991);
+	diaDeHoje(data);
+	
+	
+	return 0;
+}
+
+void diaDeHoje(data d){
+	cout << d.dia <<"/" << d.mes<< "/" << d.ano;
+}
+void init(data *d){
+	d->dia = 0;
+	d->mes = 0;
+	d->ano = 0;
+}
+void criarData(data *d, int dia, int m, int a){
+	d->dia = dia;
+	d->mes = m;
+	d->ano = a;
+}*/
+
 /*
 // STRUCT USANDO PONTEIRO Usando ponteiro 
 struct moto
