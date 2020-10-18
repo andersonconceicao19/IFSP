ae6fcdc2c993adca02c5d5cb278d46e222ae5af0
Anderson Conceição <andersonconceiicao@gmail.com>
2020-10-18 17:40:48 -0300

    conclusao do tp3

--- Java/TP3/src/Horas/horas.java
+++ Java/TP3/src/Horas/horas.java
@@ -1,10 +1,15 @@
 package Horas;
 
+import java.util.Scanner;
+
 public class horas {
 
 	private int horas;
 	private int minutos;
 	private int segundos;
+	
+	Scanner s = new Scanner(System.in);
+	private int valor;
 	
 	public horas() {
 		horas= 0;
@@ -13,34 +18,143 @@ public class horas {
 	}
 	public horas(int h, int m, int s)
 	{
-	horas = h;
-	minutos = m;
-	segundos = s;
-	
-	try {
-		validate();
-	} catch (Exception e) {
-		e.printStackTrace();
+		horas = h;
+		minutos = m;
+		segundos = s;
+		
+		try {
+			validate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
+	
+	public void setHor(int h) {
+		horas = h;
+		try {
+			validate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	public void setMin(int m) {
+		minutos = m;
+		try {
+			validate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	public void setSeg(int s) {
+		segundos = s;
+		try {
+			validate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
-	public void printar()
+	
+	public void setHor() {
+		
+		do {
+			System.out.print("Digite o valor em horas: ");			
+			valor = s.nextInt();
+		}
+		while(valor < 0 || valor > 23);
+		
+		
+		try {
+			validate();
+			horas = valor;
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+		
+	}
+	public void setMin() {
+		do {
+			System.out.print("Digite o valor em minutos: ");			
+			valor = s.nextInt();
+		}
+		while(valor < 0 || valor > 59);
+		
+		
+		try {
+			validate();
+			minutos = valor;
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public void setSeg() {
+		do {
+			System.out.print("Digite o valor em segundos: ");			
+			valor = s.nextInt();
+		}
+		while(valor < 0 || valor > 59);
+		
+		
+		try {
+			validate();
+			segundos = valor;
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public int getHor()
 	{
-		System.out.println(" hora: "+ horas +" min: "+ minutos +" seg: " + segundos);
+		return horas;
+	}
+
+	public int getMin()
+	{
+		return minutos;
+	}
+
+	public int getSeg()
+	{
+		return segundos;
+	}
+
+	public String getHora1()
+	{
+		return "" + horas + ":" + minutos + ":" + segundos;
 	}
 	
+	public String getHora2()
+	{
+		if(horas >= 0 && horas < 12) {
+			return "" + horas + ":" + minutos + ":" + segundos + " a.m" ;
+		}
+		else {
+			return "" + horas + ":" + minutos + ":" + segundos + " p.m";
+		}
+		
+	}
+	
+	public int getSegundos()
+	{
+		return ((horas * 60) * 60 ) + (minutos * 60) + segundos;		
+		
+	}
 	// Método para validar se está dentro dos padrões de hora, min. e seg.
+	
 	private void validate() throws Exception 
 	{
 		if(horas < 0 || horas > 23) 
 		{
-			 throw new Exception("Invalid value for hours");
+			 throw new Exception("ta de brincadeira né?");
 		}
-		if(minutos < 0 || minutos > 60) 
+		if(minutos < 0 || minutos > 59) 
 		{
 			 throw new Exception("Pô, meu chapa. Minutos é até 60. Ajuda ae");
 		}
-		if(segundos < 0 || segundos > 23000) 
+		if(segundos < 0 || segundos > 59 ) 
 		{
 			 throw new Exception("Segundos invalidos");
 		}
--- Java/TP3/src/Horas/progtest.java
+++ Java/TP3/src/Horas/progtest.java
@@ -1,18 +1,42 @@
 package Horas;
 
-import java.util.Scanner;
 
 public class progtest {
 
 	public static void main(String[] args)
 	{
-		// Scanner scn = new Scanner(System.in);
-		
-		 horas h = new horas(3, 60, 600);
+	 	/* Passando parametros pelo construtor */
+		 horas h = new horas(2, 00, 01);
 		 
-		 h.printar();
-				
-
+		 /* * Para entrar com valores próprios, basta descomentar este código & Comentar a instancia acima.
+		  
+		  
+		  // Sem passar parametro pelo construtor
+		  horas h = new horas();
+		 
+		// Setando valores apartir da digitação do usuario.
+		 		 
+		h.setHor();	
+		h.setMin();	
+		h.setSeg();
+		*/
+		 
+		
+		 // pegando valores das propriedades setados anteriormente.
+		int hor = h.getHor();
+		int seg = h.getSeg();
+		int min = h.getMin();		 
+		
+		// Pegando hora em formatos diferentes;
+		String x1 = h.getHora1();	
+		String x2 = h.getHora2();		
+		System.out.println(x1);
+		System.out.println(x2);
+		
+		// Retornando os segundos
+		int ss = h.getSegundos();
+		System.out.println(ss);
+		
 		
 	}
 }
